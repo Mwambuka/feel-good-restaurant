459b5a9dd27200a1fe910d3cd4275082911d6647
Bertin Mwambuka <bertinmwambuka@gmail.com>
2026-09-11 15:07:32 +0200

    new variables defined

--- discount-service.js
+++ discount-service.js
@@ -1,4 +1,9 @@
 // create discount service  
 function createDiscountService() {
   const discountService = express.Router(); 
+  const discounts = [
+    { id: 1, code: 'SUMMER10', percentage: 10 },
+    { id: 2, code: 'WINTER15', percentage: 15 },
+    { id: 3, code: 'SPRING20', percentage: 20 }
+  ];
 }
